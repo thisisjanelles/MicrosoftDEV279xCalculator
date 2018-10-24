f4d003e26458c53e9736c7cf6dfac8a9d3852e4e
Janelle <muziqlover025@gmail.com>
2018-10-24 15:34:42 -0400

    operations added

--- lab.js
+++ lab.js
@@ -10,3 +10,15 @@ var operand2 = document.getElementById("operand2");
 
 operand1.innerHTML += num1;
 operand2.innerHTML += num2;
+
+var sum = num1 + num2;
+var difference = num1 - num2;
+var product = num1 * num2;
+var quotient = num1 / num2;
+var modResult = num1 % num2
+
+document.getElementById("addition").innerHTML = "The sum of " + num1 + " and " + num2 + " is " + sum;
+document.getElementById("subtraction").innerHTML = "The difference of " + num1 + " and " + num2 + " is " + difference;
+document.getElementById("multiplication").innerHTML = "The product of " + num1 + " and " + num2 + " is " + product;
+document.getElementById("division").innerHTML = "The quotient of " + num1 + " and " + num2 + " is " + quotient;
+document.getElementById("modulus").innerHTML = "The modulus of " + num1 + " and " + num2 + " is " + modResult;
