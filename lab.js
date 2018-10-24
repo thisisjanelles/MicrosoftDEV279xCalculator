11f37bee1bd8bd97584f6a074497b9e0086b2166
Janelle <muziqlover025@gmail.com>
2018-10-24 15:27:04 -0400

    get numbers

--- lab.js
+++ lab.js
@@ -1,3 +1,12 @@
 var userName = window.prompt("Please enter your name");
 var greetingParagraph = document.getElementById("greeting");
 greetingParagraph.innerHTML += ", " + userName;
+
+var num1 = parseInt(window.prompt("Enter a number"));
+var num2 = parseInt(window.prompt("Enter another number"));
+
+var operand1 = document.getElementById("operand1");
+var operand2 = document.getElementById("operand2");
+
+operand1.innerHTML += num1;
+operand2.innerHTML += num2;
